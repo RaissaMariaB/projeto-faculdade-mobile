a66316ce6a3fef86eb91230f738e064ea77acd85
Raissa Barata Pereira <raissa.barata@ifood.com.br>
2024-06-18 23:09:36 -0300

    first commit

---
+++ app/src/main/java/br/com/projetoviacepfaculdade/MainActivity.kt
@@ -0,0 +1,53 @@
+package br.com.projetoviacepfaculdade
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.projetoviacepfaculdade.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("teste", "onCreate")
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.btnAbrir.setOnClickListener{
+            startActivity(
+                Intent(this, SecondActivity::class.java)
+            )
+        }
+        binding.btnIniciar.setOnClickListener {
+            
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        Log.i(MainActivity::class.java.simpleName, "onStart")
+
+        super.onStart()
+    }
+
+    inner class MinhaClasse: Thread() {
+         override  fun run()  {
+             super.run()
+         }
+     }
+
+
+
+}
